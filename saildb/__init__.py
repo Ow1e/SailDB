41cd9f1b278cb310c81cf59976c7184d78c20039
Owen Shaule <55895503+Ow1e@users.noreply.github.com>
2022-03-08 16:31:24 -0500

    NoneType support, Array Support, Logging and speed

--- saildb/__init__.py
+++ saildb/__init__.py
@@ -1,8 +1,14 @@
+from types import NoneType
+import logging
+
+def return_none(*args, **kwargs):
+    return None
+
 def run_type(objtype):
-    return {"i": int, "f": float, "s": str, "d": dict, "b": "BOOL"}[objtype]
+    return {"i": int, "f": float, "s": str, "d": dict, "b": "BOOL", "n": "NONE"}[objtype]
 
 def find_type(object):
-    return {int: "i", float: "f", str: "s", dict: "d", bool: "b"}[object]
+    return {int: "i", float: "f", str: "s", dict: "d", bool: "b", NoneType: "n"}[object]
 
 def parse(string : str):
     contents = string.split(":")
@@ -14,32 +20,65 @@ def parse(string : str):
     return mapping, directive, pass_through, result
 
 def load(data : str):
+    """
+    Loads SAIL into Python Objects
+    ```
+    with open("mything.sail") as f:
+        data = saildb.load(f.read())
+    ```
+    """
     export = {} # Exported Content
     mappings = {1: []}
+    line = -1
     for i in data.splitlines():
-        mapping, directive, pass_through, result = parse(i)
+        parsed = False
+        try:
+            line+=1
+            mapping, directive, pass_through, result = parse(i)
+            parsed = True
 
-        level = export
-        if mapping != 1:
-            for i in mappings[mapping]:
-                level = level[i]
+            level = export
+            if mapping != 1:
+                for i in mappings[mapping]:
+                    level = level[i]
 
-        if pass_through == dict:
-            level[directive] = {}
-            mappings[int(result)] = mappings[mapping][:] # Creates a unlinked object
-            mappings[int(result)].append(directive)
-        elif pass_through == "BOOL":
-            if result == "True":
-                level[directive] = True
+            if pass_through == dict:
+                level[directive] = {}
+                mappings[int(result)] = mappings[mapping][:] # Creates a unlinked object
+                mappings[int(result)].append(directive)
+            elif pass_through == "BOOL":
+                if result == "True":
+                    level[directive] = True
+                else:
+                    level[directive] = False
+            elif pass_through == "NONE":
+                level[directive] = None
             else:
-                level[directive] = False
-        else:
-            level[directive] = pass_through(result)
+                level[directive] = pass_through(result)
+        except Exception as e:
+            if i == "":
+                logging.warning(f"Skipped line #{line} because it was empty and gave a error")
+            elif not parsed:
+                logging.critical(f"Error parsing line {line}, elements may be missing from export")
+            else:
+                logging.critical(f"Error computing line #{line}, elements may be missing from export: {e.args}")
 
     return export
 
 def dump(data, deep = 1):
+    """
+    Dumps data into SAIL, use deep to specify how deep the database is (dont change it unless you know what you are doing)
+    """
     product = ""
+    # Check to see if its array switchable
+    if type(data) == list:
+        c = 0
+        new = {}
+        for i in data:
+            new[c] = i
+            c+=1
+        data = new
+
     for i in data:
         current = data[i]
         if type(current) == list:
--- testing.py
+++ testing.py
@@ -1,9 +1,30 @@
 from saildb import load, dump
+from datetime import datetime
+from random import randint
+from tqdm import tqdm
 
-with open("hello.sail", "w") as f:
-    f.write(dump({"array": [1, 2, 3, 4, 5, 6]}))
+def generate():
+    return {
+        "name": "Owen Shaule",
+        "email": "ow1e3@protonmail.com",
+        "time": str(datetime.now()),
+        "random": randint(1, 100000)
+    }
 
-with open("hello.sail") as f:
-    data = load(f.read())
+data = {"row": []}
+num = int(input("Rows: "))
+for i in tqdm(range(num)):
+    data["row"].append(generate())
 
-print(data)
+print("Testing generate speed")
+p = tqdm([1], total=num)
+for i in p:
+    text = dump(data)
+    p.update(num)
+
+with open("stuff.sail", "w") as f:
+    f.write(text)
+
+with open("stuff.json", "w") as f:
+    import json
+    json.dump(data, f)
